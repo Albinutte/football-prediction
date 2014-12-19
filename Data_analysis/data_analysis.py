a3370324222c5221c70ea710ebe3b77bfa3dd251
Albinutte <albina.ezus@gmail.com>
2014-12-19 04:39:48 +0300

    Plot appearance changed

--- Data_analysis/data_analysis.py
+++ Data_analysis/data_analysis.py
@@ -11,10 +11,11 @@ x_train = train.drop(['result', 'name1', 'name2'], axis=1)
 y_good = [form1 - form2 for form1, form2, res in zip(train['form1'], train['form2'], train['result']) if res == 1]
 form_dif = train['form1'] - train['form2']
 y_all = [1 / list(form_dif).count(form) for form in y_good]
+plt.figure('Form hist')
 plt.hist(y_good, weights=y_all, color='green')
 plt.xlabel('Form difference')
 plt.ylabel('Win rate')
-plt.show()
+
 
 #: goal_dif dependence
 matches_won = len([x for x in train['result'] if x == 1])
@@ -22,17 +23,20 @@ res = []
 for cnt, row in enumerate(train.values):
     dif = row[7]
     res.append(sum([x / matches_won for x, y in zip(train['result'], train['goal_diff']) if x == 1 and y < dif]))
+plt.figure('Goal diff plot')
 plt.plot(train['goal_diff'], res, '.')
 plt.xlabel('Goal diff')
 plt.ylabel('Won games rate')
-plt.show()
+
 
 #: score_dif dependence
 res = []
 for cnt, row in enumerate(train.values):
     dif = row[8]
     res.append(sum([x / matches_won for x, y in zip(train['result'], train['score_diff']) if x == 1 and y < dif]))
+plt.figure('Score diff plot')
 plt.plot(train['score_diff'], res, '.')
 plt.xlabel('Score diff')
 plt.ylabel('Won games rate')
+
 plt.show()
